9c763d0bbab73bc7f317540e23f4bec34f750a83
Jason Moore <jmoore@austwa.com>
2022-01-13 16:04:59 +0800

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Start the first process
+env > /etc/.cronenv
+sed -i 's/\"/\\"/g' /etc/.cronenv
+cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
+
+echo "Starting Cron"
+service cron start &
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start cron: $status"
+  exit $status
+fi
+
+/bin/bash
